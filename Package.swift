331abecb53db805d9be50786f3aa6202b06818a9
Kostian Serhii <>
2019-08-19 15:34:24 +0300

    updated Package.swift

--- Package.swift
+++ Package.swift
@@ -9,8 +9,9 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ContactsPermissions",
-            path: "ContactsPermissions/Sources")
-            targets: ["ContactsPermissions"]),
+            path: "ContactsPermissions/Sources"),
+            targets: ["ContactsPermissions"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ContactsPermissions",
-            dependencies: [])
-        ),
+            dependencies: []
+        )
     ]
 )
